e6f38959edf48a7a2311e8fd979df9cdcb162d92
maniksheikh <mdmanik081498@gmail.com>
2023-11-07 13:11:01 +0600

    added the search input page

--- components/car/SearchBar.vue
+++ components/car/SearchBar.vue
@@ -6,12 +6,23 @@
             type="text"
             class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
             placeholder="Search by city..."
+            :class=" cityError ? 'border border-red-900' : ''"
+            v-model="city"
           />
-          <button class="bg-sky-500 px-10 text-white">Search</button>
+          <button class="bg-sky-500 px-10 text-white" @click="handleSearch()">Search</button>
         </div>
 </template>
 
-<script>
- 
+<script setup>
+   const city =ref("");
+   const cityError =ref(false);
+
+   const handleSearch = () => {
+    if(!city.value) {
+      alert('Please insert your car name')
+      return cityError.value = true
+    }
+    navigateTo(`/city/${city.value}/car`)
+   }
 </script>
 

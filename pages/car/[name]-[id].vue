258714a11286efc85d3c8838a49330cf1b51ff82
maniksheikh <mdmanik081498@gmail.com>
2023-11-08 01:04:45 +0600

    created the custom page and use composables utilities

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,25 +1,22 @@
 <template>
   <div>
-    <div
-      class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
-        <CarDetailsHero></CarDetailsHero>
-        <CarDetailsAttributes></CarDetailsAttributes>
-         <CarDetailsDescription></CarDetailsDescription>
-         <CarDetailsContact></CarDetailsContact>
-    </div>
+    <CarDetailsHero></CarDetailsHero>
+    <CarDetailsAttributes></CarDetailsAttributes>
+    <CarDetailsDescription></CarDetailsDescription>
+    <CarDetailsContact></CarDetailsContact>
   </div>
 </template>
 
 <script setup>
-  const route =useRoute();
+const route = useRoute();
+const {toTitleCase} = useUtilities();
+useHead({
+  title: toTitleCase(route.params.name),
+});
 
-  useHead({
-    title: toTitleCase(route.params.name)
-  })
+definePageMeta({
+  layout: "custom",
+});
 
-  function toTitleCase(str)  {
-       return str.replace(/\w\S*/g, function(txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-       });
-   }
+
 </script>
--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -1,22 +1,23 @@
 <template>
-  <div
-    class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
-    <div class="mt-32 flex">
-      <CarSideBar></CarSideBar>
-      <NuxtPage></NuxtPage>
-    </div>
+  <div class="mt-32 flex">
+    <CarSideBar></CarSideBar>
+    <NuxtPage></NuxtPage>
   </div>
 </template>
 
 <script setup>
-   const route = useRoute();
-   useHead({
-    title:`${route.params.make ? toTitleCase(route.params.make) : 'Cars'} in ${toTitleCase(route.params.city)}`
-   });
+const route = useRoute();
+const {toTitleCase} = useUtilities();
 
-   function toTitleCase(str)  {
-       return str.replace(/\w\S*/g, function(txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-       });
-   }
+useHead({
+  title: `${
+    route.params.make ? toTitleCase(route.params.make) : "Cars"
+  } in ${toTitleCase(route.params.city)}`,
+});
+
+definePageMeta({
+  layout: "custom",
+});
+
+
 </script>
